8b08a9ac64e16bc19e8d4005572f7ccd9c918401
Adjani <133295232+Adjani09@users.noreply.github.com>
2024-01-26 21:52:37 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,130 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	return (
+		<>
+			<div className="container">
+				<h1 className="text-danger">Characters</h1>
+				<div className="my-carousel">
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<div className="button-footer">
+							<button>Learn more</button>
+							<button>C</button>
+						</div>
+					</div>
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<div className="button-footer">
+							<button>Learn more</button>
+							<button>C</button>
+						</div>
+					</div>
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<div className="button-footer">
+							<button>Learn more</button>
+							<button>C</button>
+						</div>
+					</div>
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<div className="button-footer">
+							<button>Learn more</button>
+							<button>C</button>
+						</div>
+					</div>
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<div className="button-footer">
+							<button>Learn more</button>
+							<button>C</button>
+						</div>
+					</div>
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<div className="button-footer">
+							<button>Learn more</button>
+							<button>C</button>
+						</div>
+					</div>
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<button>Learn more</button>
+						<button>C</button>
+					</div>
+					<div className="myCard">
+						<div>
+							<img src="https://picsum.photos/id/237/200/300" alt="" />
+						</div>
+						<div>
+							<p>Nombre</p>
+							<p>Gender</p>
+							<p>Hair Color</p>
+							<p>Eye Color</p>
+						</div>
+						<button>Learn more</button>
+						<button>C</button>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+};
