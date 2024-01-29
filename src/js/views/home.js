b1a1d84fba0455956fa2d7ea55d66674c2dc87ff
Adjani <133295232+Adjani09@users.noreply.github.com>
2024-01-29 21:09:03 +0000

    commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,122 +9,39 @@ export const Home = () => {
 				<div className="my-carousel">
 					<div className="myCard">
 						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
+							<img src="https://picsum.photos/id/237/200/200" alt="" />
 						</div>
-						<div>
+						<div className="body-text">
 							<p>Nombre</p>
 							<p>Gender</p>
 							<p>Hair Color</p>
 							<p>Eye Color</p>
 						</div>
-						<div className="button-footer">
-							<button>Learn more</button>
-							<button>C</button>
+						<div className="button-footer" style={{ justifyContent: "space-between", display: "flex" }}>
+							<button className="btn btn-outline-primary">Learn more</button>
+							<button className="btn btn-outline-warning">C</button>
 						</div>
-					</div>
-					<div className="myCard">
-						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
-						</div>
-						<div>
-							<p>Nombre</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
-						<div className="button-footer">
-							<button>Learn more</button>
-							<button>C</button>
-						</div>
-					</div>
-					<div className="myCard">
-						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
-						</div>
-						<div>
-							<p>Nombre</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
-						<div className="button-footer">
-							<button>Learn more</button>
-							<button>C</button>
-						</div>
-					</div>
-					<div className="myCard">
-						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
-						</div>
-						<div>
-							<p>Nombre</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
-						<div className="button-footer">
-							<button>Learn more</button>
-							<button>C</button>
-						</div>
-					</div>
-					<div className="myCard">
-						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
-						</div>
-						<div>
-							<p>Nombre</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
-						<div className="button-footer">
-							<button>Learn more</button>
-							<button>C</button>
-						</div>
-					</div>
-					<div className="myCard">
-						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
-						</div>
-						<div>
-							<p>Nombre</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
-						<div className="button-footer">
-							<button>Learn more</button>
-							<button>C</button>
-						</div>
-					</div>
-					<div className="myCard">
-						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
-						</div>
-						<div>
-							<p>Nombre</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
-						<button>Learn more</button>
-						<button>C</button>
-					</div>
-					<div className="myCard">
-						<div>
-							<img src="https://picsum.photos/id/237/200/300" alt="" />
-						</div>
-						<div>
-							<p>Nombre</p>
-							<p>Gender</p>
-							<p>Hair Color</p>
-							<p>Eye Color</p>
-						</div>
-						<button>Learn more</button>
-						<button>C</button>
 					</div>
 				</div>
 			</div>
-		</>
-	)
+				<div className="container mt-5">
+					<h1 className="text-danger">Planets</h1>
+					<div className="my-carousel">
+						<div className="myCard">
+							<div>
+								<img src="https://picsum.photos/id/237/200/200" alt="" />
+							</div>
+							<div className="body-text">
+								<p>Population:</p>
+								<p>Terrain:</p>
+							</div>
+							<div className="button-footer" style={{ justifyContent: "space-between", display: "flex" }}>
+								<button className="btn btn-outline-primary">Learn more</button>
+								<button className="btn btn-outline-warning">C</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</>
+			)
 };
