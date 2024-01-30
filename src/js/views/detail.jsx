7ef1938f546fd23564c823e7719bac6e655745e0
Adjani <133295232+Adjani09@users.noreply.github.com>
2024-01-30 19:16:25 +0000

    commit

--- src/js/views/detail.jsx
+++ src/js/views/detail.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../store/appContext";
 import { useParams } from "react-router";
 
 
 const Detail = () => {
     const params = useParams()
-    return(
+    const [search, setSearch] = useState({})
+    const { store } = useContext(Context)
+
+    const details = () => {
+        if (params.nature == "characters") {
+            const searchFind = store.characters.find((item) => item._id == params.id)
+            setSearch(searchFind)
+        }else {
+            const searchFind = store.planets.find((item) => item._id == params.id)
+            setSearch(searchFind);
+        } 
+        
+    }
+
+    useEffect(() => {
+        details()
+    }, [])
+
+    return (
         <>
-        <h1>Hola que tal</h1>
+            <h1>{search?.properties?.name}</h1>
         </>
     )
 }
